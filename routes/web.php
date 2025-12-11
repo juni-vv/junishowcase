4c5a2224489558bda5d668a636371cddf0f358f4
fecidgames <mynameisdogwoofwoof@gmail.com>
2025-12-11 13:19:07 +0100

    Downloads blank page

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,8 @@ Route::get('/about', [App\Http\Controllers\AgeController::class, 'about']);
 Route::get('/fox', function () {
     return view('fox');
 });
+Route::get('/downloads', function () {
+    return view('downloads');
+});
 
 Route::get('/', [App\Http\Controllers\MainController::class, 'home']);
